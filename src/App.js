553dea496eb6e93b7953cf24cbdb4018cbe80e31
Alexander Scherer <alexscherer@outlook.com>
2019-09-12 15:47:46 +0200

    start syling with css

--- src/App.js
+++ src/App.js
@@ -34,11 +34,13 @@ function Header() {
   const [status, setStatus] = useState(true);
 
   return (
-    <header>
+    <header className="container">
       <h1>RANDOMO - THE Color Generator</h1>
-      <button onClick={() => setStatus(false)}>What?</button>
-      <button onClick={() => setStatus(true)}>X</button>
-      <p hidden={status}>Bla bla bla balalala</p>
+      <div className="infobox">
+        <button onClick={() => setStatus(false)}>What?</button>
+        <button onClick={() => setStatus(true)}>X</button>
+        <p hidden={status}>Bla bla bla balalala</p>
+      </div>
     </header>
   );
 }
@@ -46,6 +48,7 @@ function Header() {
 function ColorGenerator() {
   const [color, setColor] = useState('#ffffff');
   const [myhue, setHue] = useState('random');
+  const [mylum, setlum] = useState('random');
   let styles = {
     margin: '20px',
     width: '250px',
@@ -54,13 +57,14 @@ function ColorGenerator() {
   };
 
   return (
-    <>
+    <div className="container">
       <button
+        className="buttonStaunen"
         onClick={() => {
           let randomColor = require('randomcolor');
           setColor(
             randomColor({
-              luminosity: 'light',
+              luminosity: mylum,
               hue: myhue
             })
           );
@@ -81,8 +85,24 @@ function ColorGenerator() {
         <option value="red">Red</option>
         <option value="blue">Blue</option>
         <option value="green">Green</option>
+        <option value="orange">Orange</option>
+        <option value="yellow">Yellow</option>
+        <option value="purple">Purple</option>
+        <option value="pink">Pink</option>
+        <option value="monochrome">Monochrome</option>
       </select>
-    </>
+
+      <select
+        name="luminosity"
+        value={mylum}
+        onChange={event => setlum(event.target.value)}
+      >
+        <option value="random">Random</option>
+        <option value="light">Light</option>
+        <option value="dark">Dark</option>
+        <option value="bright">Bright</option>
+      </select>
+    </div>
   );
 }
 
